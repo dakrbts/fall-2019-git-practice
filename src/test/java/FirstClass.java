b65e0d3c5f49bfbd8f09549f107b4690ea377bec
dakrbts <dakrbts@gmail.com>
2020-04-01 19:37:33 -0400

    update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,6 +2,8 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("brave new world");
         System.out.println("someone"); //comment
+        System.out.println("brave new world"); //comment
+        System.out.println("someone");
         System.out.println("change one");
         System.out.println("change two");
         System.out.println("change three");
